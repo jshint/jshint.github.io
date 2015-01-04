07389f58e1717915dd60cf1900eb2ec5c2827121
Mike Pennisi <mike@mikepennisi.com>
2015-01-03 22:49:31 -0500

    Generate "Contributing" page from project file
    Use JSHint's `CONTRIBUTING.md` file to generate the "Contributing" page.
    Inject the contents using a new Oddweb plugin for Mustache-style
    templating, and re-implement JSHint version number injection using the
    new plugin.

---
+++ plugins/variables.js
@@ -0,0 +1,61 @@
+/**
+ * @file - Resolve any variables declared within Moustache templating
+ * directives.
+ */
+"use strict";
+var path = require("path");
+var fs = require("fs");
+
+var pkg = require(path.join(
+  __dirname, "..", "node_modules", "jshint", "package.json")
+);
+var contributing = fs.readFileSync(
+  "res/jshint/CONTRIBUTING.md", { encoding: "utf-8" }
+);
+var context = {
+  contributionGuidelines: translateFencedCode(contributing),
+  version: pkg.version
+};
+
+/**
+ * The Markdown preprocessor used by Oddweb does not support the
+ * "backtick"/"code fence" syntax for multiline code examples. If present,
+ * these must be translated to the "indented" format.
+ */
+function translateFencedCode(markdown) {
+  var openPattern = /^(\s*)```[\w]*\s*$/;
+  var closePattern = /^\s*```\s*$/;
+  var lines = markdown.split("\n");
+  var state = { fixed: [], indent: '', isCode: false };
+
+  lines.reduce(function(state, line) {
+    var match;
+    if (state.isCode) {
+      if (closePattern.test(line)) {
+        state.isCode = false;
+      } else {
+        state.fixed.push(state.indent + line);
+      }
+    } else {
+      match = openPattern.exec(line);
+      if (match) {
+        state.isCode = true;
+        state.indent = match[1] || '    ';
+      } else {
+        state.fixed.push(line);
+      }
+    }
+    return state;
+  }, state);
+
+  return state.fixed.join("\n");
+}
+
+module.exports = function (site, handlebars) {
+  site.pages.forEach(function(page) {
+    var template = handlebars.compile(page.data);
+    page.data = template(context);
+  });
+
+  return site;
+};
